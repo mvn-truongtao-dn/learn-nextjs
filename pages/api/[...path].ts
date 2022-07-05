3f6ea10ce267497fe053d3119999babc41cc6642
Truong Tao Q. VN.Danang <truong.tao@MBA020236P.local>
2022-07-05 09:13:13 +0700

    update

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -15,12 +15,14 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  //don't send cookies to api server
+  //don't send cookies to api se    rver
   req.headers.cookie = '';
   // api/students
   // https://js-post-api.herokuapp.com/api/students
   proxy.web(req, res, {
-    target: process.env.API_URL,
+    // target: process.env.API_URL,
+    target: 'https://js-post-api.herokuapp.com',
+
     changeOrigin: true,
     selfHandleResponse: false,
   });
